ddf8dcc9666e4fb9249588f4dae17df6a14069a7
Daryl <darylvdberg@gmail.com>
2019-09-27 14:00:53 +0200

    - Started working on layout for the MainActivity

--- app/src/main/java/com/example/soullinkhelper/MainActivity.java
+++ app/src/main/java/com/example/soullinkhelper/MainActivity.java
@@ -2,14 +2,31 @@ package com.example.soullinkhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView linkPokemonBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
+        setTheme(R.style.NewGameScreen);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linkPokemonBtn = findViewById(R.id.linkPokemonBtn);
+        linkPokemonBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                linkPokemonActivity();
+            }
+        });
+    }
+
+    private void linkPokemonActivity(){
+        startActivity(new Intent(this, LinkPokemon.class));
     }
 }

e7a514e054562a380047dc71d7642ea2affe3fe3
Daryl <darylvdberg@gmail.com>
2019-10-01 17:17:11 +0200

    - Temp commit

--- app/src/main/java/com/example/soullinkhelper/MainActivity.java
+++ app/src/main/java/com/example/soullinkhelper/MainActivity.java
@@ -6,16 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.soullinkhelper.models.PairManager;
+import com.example.soullinkhelper.models.PlayerManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    // TODO: Get correct games
+    // TODO: Show all linked pokemon
+    // TODO: Show Player name
+
     private ImageView linkPokemonBtn;
+    private TextView noLinksFound;
+    private PairManager pairManager;
+    private PlayerManager playerManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.NewGameScreen);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pairManager = PairManager.getInstance();
 
         linkPokemonBtn = findViewById(R.id.linkPokemonBtn);
         linkPokemonBtn.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
                 linkPokemonActivity();
             }
         });
+
+        noLinksFound = findViewById(R.id.noLinksFound);
+        noLinksFound.setVisibility(pairManager.getPairList().size() < 1 ?
+                View.VISIBLE :
+                View.GONE);
     }
 
     private void linkPokemonActivity(){

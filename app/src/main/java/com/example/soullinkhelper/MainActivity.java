7028188c6a14b0b614b5fb6e609a84f1ca4712c7
Ryan Rooijakkers <R.Rooijakkers@student.tudelft.nl>
2019-10-09 20:23:58 +0200

    Oopsie worked in development,
    Made pair background

--- app/src/main/java/com/example/soullinkhelper/MainActivity.java
+++ app/src/main/java/com/example/soullinkhelper/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.NewGameScreen);
+        setTheme(R.style.SplashScreen);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
--- app/src/main/java/com/example/soullinkhelper/adapter/LinkAdapter.java
+++ app/src/main/java/com/example/soullinkhelper/adapter/LinkAdapter.java
@@ -1,17 +1,30 @@
 package com.example.soullinkhelper.adapter;
 
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.soullinkhelper.R;
+import com.example.soullinkhelper.enums.State;
 import com.example.soullinkhelper.models.Pair;
 
 import org.w3c.dom.Text;
@@ -30,6 +43,7 @@ public class LinkAdapter extends RecyclerView.Adapter<LinkAdapter.ViewHolder> {
         private TextView pokemonType2;
         private TextView pokemonState;
         private TextView caughtRoute;
+        private LinearLayout pairLayout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -41,6 +55,7 @@ public class LinkAdapter extends RecyclerView.Adapter<LinkAdapter.ViewHolder> {
             pokemonType2 = itemView.findViewById(R.id.pokemon_type_2);
             pokemonState = itemView.findViewById(R.id.pokemon_state);
             caughtRoute = itemView.findViewById(R.id.route_caught);
+            pairLayout = itemView.findViewById(R.id.linearLayoutPair);
         }
     }
 
@@ -52,9 +67,6 @@ public class LinkAdapter extends RecyclerView.Adapter<LinkAdapter.ViewHolder> {
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.link_list, parent, false);
-
-
-
      return new ViewHolder(v);
     }
 
@@ -74,6 +86,21 @@ public class LinkAdapter extends RecyclerView.Adapter<LinkAdapter.ViewHolder> {
         holder.pokemonType2.setText(TextUtils.join(" / ", pairArrayList.get(position).getPokemon2().getTypes()));
         holder.pokemonState.setText(pairArrayList.get(position).getState().toString());
         holder.caughtRoute.setText(pairArrayList.get(position).getRoute());
+
+        ColorMatrix matrix = new ColorMatrix();
+        matrix.setSaturation(0);
+        ColorMatrixColorFilter filterDead = new ColorMatrixColorFilter(matrix);
+        matrix.setSaturation(1);
+        ColorMatrixColorFilter filterAlive = new ColorMatrixColorFilter(matrix);
+
+        holder.pokemon1.setColorFilter((pairArrayList.get(position).getState().equals(State.DEAD)) ?
+                filterDead : filterAlive);
+        holder.pokemon2.setColorFilter((pairArrayList.get(position).getState().equals(State.DEAD)) ?
+                filterDead : filterAlive);
+
+        holder.pairLayout.setBackground((pairArrayList.get(position).getState().equals(State.DEAD)) ?
+                ContextCompat.getDrawable(holder.itemView.getContext(), R.mipmap.link_background_dead):
+                ContextCompat.getDrawable(holder.itemView.getContext(), R.mipmap.link_background));
     }
 
     @Override

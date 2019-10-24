5aeeb60acf5cd705d9c56a9731fd77c21aa6a4a5
Daryl <darylvdberg@gmail.com>
2019-10-24 18:28:25 +0200

    resize image to fit smaller screen.

--- app/src/main/java/com/example/soullinkhelper/LinkPokemon.java
+++ app/src/main/java/com/example/soullinkhelper/LinkPokemon.java
@@ -1,14 +1,18 @@
 package com.example.soullinkhelper;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.soullinkhelper.fragments.LinkPokemonFragment;
